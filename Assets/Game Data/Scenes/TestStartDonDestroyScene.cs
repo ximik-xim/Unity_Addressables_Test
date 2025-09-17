f72c76699491f6b8fcfbdd2d8d94f81068e2336b
NO-243PCBER03I9\ENP <ximik-xim@mail.ru>
2025-09-17 16:40:45 +0300

    - Исправил баг - Сделал тестовую сборку (пока что то идет не так, не берет из локального бандла сцену....)

--- Assets/Game Data/Scenes/TestStartDonDestroyScene.cs
+++ Assets/Game Data/Scenes/TestStartDonDestroyScene.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
-public class sssss : MonoBehaviour
+public class TestStartDonDestroyScene : MonoBehaviour
 {
     [SerializeField]
     private AssetReference _key;

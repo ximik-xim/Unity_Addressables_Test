bd161250c5cb9a61d8c1465886a181ca1e1d24e1
NO-243PCBER03I9\ENP <ximik-xim@mail.ru>
2025-10-05 20:06:45 +0300

    - Переделал способ указ ссылки на сцену помеч. как addressable(т.к прошлый способ ссылался на класс Unity Editor) - Сделал тестовую сборку

--- Assets/Scene Logic/Key Name Scene/Logic Storage Key Ref Scene/AssetReferenceSceneCustom.cs
+++ Assets/Scene Logic/Key Name Scene/Logic Storage Key Ref Scene/AssetReferenceSceneCustom.cs
@@ -4,9 +4,36 @@ using UnityEngine.AddressableAssets;
 using UnityEngine.SceneManagement;
 
 [System.Serializable]
-public class AssetReferenceSceneCustom : AssetReferenceT<SceneAsset>
+public class AssetReferenceSceneCustom : AssetReference
 {
     public AssetReferenceSceneCustom(string guid) : base(guid)
     {
     }
+    
+#if UNITY_EDITOR
+    //Проверка через путь(вызывается при выборе из поиска)
+    //Оставляет только сцены, помеченные как Addressable.
+    public override bool ValidateAsset(string path)
+    {
+        // Проверяем, является ли ассет сценой
+        var asset = UnityEditor.AssetDatabase.LoadAssetAtPath<UnityEditor.SceneAsset>(path);
+        if (asset == null)
+        {
+            return false;
+        }
+        
+        // Проверяем, помечена ли сцена как Addressable
+        var guid = UnityEditor.AssetDatabase.AssetPathToGUID(path);
+        
+        return UnityEditor.AddressableAssets.AddressableAssetSettingsDefaultObject.Settings.FindAssetEntry(guid) != null;
+    }
+    
+    /// Проверка по объекту(вызывается при drag & drop)
+    public override bool ValidateAsset(Object obj)
+    {
+        // Дополнительная проверка (на всякий случай)
+        return obj is UnityEditor.SceneAsset scene && UnityEditor.AddressableAssets.AddressableAssetSettingsDefaultObject.Settings
+                   .FindAssetEntry(UnityEditor.AssetDatabase.AssetPathToGUID(UnityEditor.AssetDatabase.GetAssetPath(scene))) != null;
+    }
+#endif
 }

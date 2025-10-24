e1de1fd8f7c5b4b34d622c4ca8b247e0d447c06d
NO-243PCBER03I9\ENP <ximik-xim@mail.ru>
2025-10-24 15:59:25 +0300

    - Переделал логику загрузки запасных сцен(теперь они НЕ будут Addressable, и будут вшиты в игру) - В плагин Scene Level добавил скрипт который сначала пытаеться загрузить Remote сцену через Addressable, затем Local сцену через SceneManager - Сделал бэкап

--- Assets/Plugins/Scene Level Manager/Addressable Scene Scripts/Key Name Scene/Logic Storage Key Ref Scene/Storage/TList/SO_Data_KeyReferenceScene.cs
+++ Assets/Plugins/Scene Level Manager/Addressable Scene Scripts/Key Name Scene/Logic Storage Key Ref Scene/Storage/TList/SO_Data_KeyReferenceScene.cs
@@ -1,7 +1,7 @@
 using UnityEngine; 
 using TListPlugin; 
 
-[CreateAssetMenu(menuName = "Logic Scene Level/Storage/Storage Key Ref Scene")]
+[CreateAssetMenu(menuName = "Scene Level Manager/Storage/Storage Key Ref Scene")]
 public class SO_Data_KeyReferenceScene : Abs_SO_IdentifierAndData<IdentifierAndData_KeyReferenceScene,IndifNameSO_KeyReferenceScene,string,KeyReferenceScene>
 {
 
--- Assets/Plugins/Scene Level Manager/Default Scene Scripts/Key Name Scene/Storage Key Name Scene/TList/SO_Data_NameSceneAndKeyString.cs
+++ Assets/Plugins/Scene Level Manager/Default Scene Scripts/Key Name Scene/Storage Key Name Scene/TList/SO_Data_NameSceneAndKeyString.cs
@@ -1,6 +1,6 @@
 using UnityEngine; 
 using TListPlugin; 
-[CreateAssetMenu(menuName = "Logic Scene Level/Storage/Storage Name Scene And Key String")]
+[CreateAssetMenu(menuName = "Scene Level Manager/Storage/Storage Name Scene And Key String")]
 public class SO_Data_NameSceneAndKeyString : Abs_SO_IdentifierAndData<IdentifierAndData_NameSceneAndKeyString,IndifNameSO_NameSceneAndKeyString,string,NameSceneAndKeyString>
 {
 
